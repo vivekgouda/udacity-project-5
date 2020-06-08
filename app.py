98bf27dcdd271b83ed3ea0d3cd52efb81190f01c
Vivek Gouda <vivekgouda2@gmail.com>
2020-06-08 15:49:55 +0000

    updated app.py

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = f"<h3>Sklearn Prediction Home</h3>"
+    dummy=""
+    html = f"<h3>Sklearn Prediction Home</h3>{dummy}"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
